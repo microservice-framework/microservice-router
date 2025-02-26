65d8b5b073449a7653b2dcc1bd4c6976e06d48e2
Gor Martsen <gor.martsen@gmail.com>
2025-02-25 22:22:28 -0500

    preparation

--- router-admin.js
+++ router-admin.js
@@ -3,64 +3,94 @@
  */
 'use strict';
 
-const framework = '@microservice-framework';
-const Cluster = require(framework + '/microservice-cluster');
-const Microservice = require(framework + '/microservice');
-const MicroserviceRouterRegister = require(framework + '/microservice-router-register').register;
-const MongoClient = require('mongodb').MongoClient;
-const debugF = require('debug');
+import { ClientRegister } from '@microservice-framework/microservice-router-register';
+import Microservice from '@microservice-framework/microservice';
+import Cluster from '@microservice-framework/microservice-cluster';
+import debugF from 'debug';
 
-var debug = {
+const debug = {
   log: debugF('router:log'),
   debug: debugF('router:debug')
 };
-
-require('dotenv').config();
-
-
-var MongoURL = '';
-if (process.env.MONGO_URL) {
-  MongoURL = MongoURL + process.env.MONGO_URL;
-}
-
-if (process.env.MONGO_PREFIX) {
-  MongoURL = MongoURL + process.env.MONGO_PREFIX;
-}
-
-if (process.env.MONGO_OPTIONS) {
-  MongoURL = MongoURL + process.env.MONGO_OPTIONS;
-}
-
-
-var mservice = new Microservice({
-  mongoUrl: MongoURL,
-  mongoTable: process.env.MONGO_TABLE,
-  secureKey: process.env.SECURE_KEY,
-  schema: process.env.SCHEMA
-});
-
-var mControlCluster = new Cluster({
-  pid: process.env.PIDFILE,
-  port: process.env.PORT,
-  count: process.env.WORKERS,
-  hostname: process.env.HOSTNAME,
-  callbacks: {
-    validate: mservice.validate,
-    POST: adminPOST,
-    GET: mservice.get,
-    PUT: mservice.put,
-    DELETE: mservice.delete,
-    SEARCH: adminSearch,
-    OPTIONS: mservice.options,
-    shutdown: adminShutdown,
-    init: adminInit,
-  }
-});
 
 let interval = 6000;
 if (process.env.INTERVAL) {
   interval = process.env.INTERVAL;
 }
+
+// Load environment variables from .env file
+import dotenv from 'dotenv';
+dotenv.config();
+
+// Create a new microservice
+let mservice = new Microservice({
+  mongoUrl: process.env.MONGO_URL,
+  mongoDB: process.env.MONGO_DB,
+  schema: process.env.SCHEMA,
+  mongoTable: process.env.MONGO_TABLE,
+  secureKey: process.env.SECURE_KEY
+});
+
+new Cluster({
+  singleton: RegisterLoader,
+  validate: mservice.validate.bind(mservice),
+  methods: {
+    POST: async function (data, request) {
+      if (!data.online) {
+        data.online = true
+      }
+      return mservice.post(data, request)
+    },
+    GET: mservice.get.bind(mservice),
+    PUT: mservice.put.bind(mservice),
+    DELETE: mservice.delete.bind(mservice),
+    SEARCH: mservice.search.bind(mservice),
+    OPTIONS: mservice.options.bind(mservice),
+  },
+});
+
+
+const cleanupExpired = async function () {
+  debug.debug('cleanup tokens');
+  let searchToken = {
+    expireAt: { $lt: Date.now() },
+  };
+  let request = {
+    headers: {},
+  };
+  let response = await mservice.search(searchToken, request);
+  if (response.code != 404) {
+    for (let token of response.answer) {
+      debug.debug('found token %O', token);
+      let deleteResponse = await mservice.delete(token.accessToken, request);
+      debug.debug('deleted token %O', deleteResponse);
+    }
+  }
+};
+
+function RegisterLoader(isStart, variables) {
+  let cluster = this;
+  debug.debug('RegisterLoader');
+  if (isStart) {
+    let register = new ClientRegister({
+      route: {
+        path: ['register'],
+        url: process.env.SELF_URL,
+        secureKey: process.env.SECURE_KEY,
+      },
+      cluster: cluster.cluster,
+    });
+    let interval = setInterval(() => {
+      cleanupExpired();
+    }, process.env.ROUTER_PERIOD);
+    variables({ register: register, interval: interval });
+  } else {
+    debug.debug('stop cleaner');
+    clearInterval(variables.interval);
+    variables.register.shutdown();
+  }
+}
+
 
 var mserviceRegister = new MicroserviceRouterRegister({
   server: {
@@ -103,46 +133,6 @@ function adminShutdown(cleanRouteTableInerval){
   }
 }
 
-/**
- * POST Handler.
- */
-function adminPOST(jsonData, requestDetails, callback) {
-  // Version 1.x compatibility.
-  if (!jsonData.type) {
-    jsonData.type = "handler"
-  }
-  // Version 1.x  websocket compatibility.
-  if (jsonData.path && jsonData.path.indexOf('ws') != -1) {
-    jsonData.type = "websocket"
-  }
-  if (!jsonData.online) {
-    jsonData.online = true
-  }
-  mservice.post(jsonData, requestDetails, callback)
-}
-
-/**
- * SEARCH handler.
- */
-function adminSearch(jsonData, requestDetails, callback) {
-  // Version 1.x compatibility.
-  if (jsonData.query) {
-    if (!jsonData.query.type) {
-      jsonData.query.type = { $eq: 'handler'}
-    }
-    if (!jsonData.query.online) {
-      jsonData.query.online = true
-    }
-  } else {
-    if (!jsonData.type) {
-      jsonData.type = { $eq: 'handler'}
-    }
-    if (!jsonData.online) {
-      jsonData.online = true
-    }
-  }
-  mservice.search(jsonData, requestDetails, callback)
-}
 
 /**
  * Update route infor each 10 sec.
