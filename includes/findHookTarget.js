862ed1224403bd48489fa51c6e5f05d2ef615b12
Gor Martsen <gor.martsen@gmail.com>
2025-02-26 22:14:28 -0500

    Use object hook instead of single params

--- includes/findHookTarget.js
+++ includes/findHookTarget.js
@@ -8,36 +8,38 @@ import findAllTargets from './findAllTargets.js';
  *
  *
  * hook {
- * phase:
- * type:
- * group:
+ *   phase: before, after
+ *   type: broadcast, notify
+ *   group:
  * }
  * options:
  * 
  *  */
-export default function (hook, options, phase, type, group) {
-  debug.debugHook('Find all hooks route: %s phase: %s type: %s group: %s', options.route, phase, type, group);
-  let allHookTargets = findAllTargets('hook', options);
-  if (allHookTargets instanceof Error) {
-    return allHookTargets;
+export default function (hook, options) {
+  debug.debugHook('Find all hooks route: %s phase: %s type: %s group: %s', options.route, hook.phase, hook.type, hook.group);
+  
+  let allHookEndpoints = findAllTargets('hook', options);
+  if (allHookEndpoints instanceof Error) {
+    return allHookEndpoints;
   }
   let finalHookTable = [];
-  for (let target of allHookTargets) {
+  for (let target of allHookEndpoints) {
     // skip hooks with no hook properties
     if (!target.hook || !target.hook.length) {
       continue;
     }
-    for (let hook of target.hook) {
-      if (phase !== null && hook.phase !== phase) {
+    for (let hookItem of target.hook) {
+      if (hook.phase !== null && hookItem.phase !== hook.phase) {
         continue;
       }
-      if (hook.type !== type) {
+      if (hookItem.type !== hook.type) {
         continue;
       }
+      //make a copy of the target endpoint
       let targetCopy = JSON.parse(JSON.stringify(target));
       delete targetCopy.hook;
-      if (hook.group) {
-        targetCopy.group = hook.group;
+      if (hookItem.group) {
+        targetCopy.group = hookItem.group;
       } else {
         targetCopy.group = '_default';
       }
@@ -45,15 +47,17 @@ export default function (hook, options, phase, type, group) {
       finalHookTable.push(targetCopy);
     }
   }
-  if (typeof group !== 'undefined') {
+  // if endpoint is group based, filter to single endpoint
+  if (typeof hook.group !== 'undefined') {
     finalHookTable = finalHookTable.filter(function (elem) {
-      return elem.group == group;
+      return elem.group == hook.group;
     });
   }
+  // no hook endpoints, return error
   if (!finalHookTable.length) {
     debug.debug('Not found for %s', options.route);
-    debug.log('Hook instance %s not found', group);
-    debug.debugHook('Hook instance %s not found', group);
+    debug.log('Hook instance %s not found', hook.group);
+    debug.debugHook('Hook instance %s not found', hook.group);
     return new Error('Hook instance not found');
   }
   return finalHookTable;
