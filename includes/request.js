40a1d77a981766ffb71a6a47255280ffd376c2ac
Gor Martsen <Gor.martsen@gmail.com>
2025-02-27 17:00:25 -0500

    disable parse

--- includes/request.js
+++ includes/request.js
@@ -12,6 +12,9 @@ export default async function (requestOptions) {
     return err;
   }
 
+  // disable parse JS
+  requestOptions.transformResponse = (x) => x;
+
   debug.debug('requestOptions', requestOptions);
 
   return axios
